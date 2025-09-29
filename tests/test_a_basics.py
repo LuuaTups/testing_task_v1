97d9d9e6cff74da1c6c410d614dac54122dfc7bf
Robin Rattasep <Robin.Rattasep@tthk.ee>
2025-09-29 11:12:35 +0300

    test

--- tests/test_a_basics.py
+++ tests/test_a_basics.py
@@ -13,6 +13,7 @@ def test_add_basic():
     assert add(2, 3) == 5
     assert add(0, 0) == 0
     assert add(-1, 1) == 0
+    assert add(-2, -3) == -5
 
 def test_sub_basic():
     """Test lahutamise funktsiooni."""
